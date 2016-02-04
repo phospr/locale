ed58e61c405b5032e5ef58cc3ace4d13977b2265
Tom Haskins-Vaughan <tomhv@janeiredale.com>
2016-02-04 08:39:47 -0500

    First draft

---
+++ src/Locale.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * This file is part of the Phospr Locale package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phospr;
+
+/**
+ * A Value Object representation of 5 character locale string:
+ *
+ *     * en_US
+ *     * fr_FR
+ *     * en_GB
+ *     * etc
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  1.0.0
+ */
+class Locale
+{
+    /**
+     * languageCode
+     *
+     * @var string
+     */
+    private $languageCode;
+
+    /**
+     * countryCode
+     *
+     * @var string
+     */
+    private $countryCode;
+
+    /**
+     * Constructor
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @param string $languageCode
+     * @param string $countryCode
+     */
+    private function __construct($languageCode, $countryCode)
+    {
+        $this->languageCode = strtolower($languageCode);
+        $this->countryCode = strtoupper($countryCode);
+    }
+
+    /**
+     * To string
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf(
+            '%s_%s',
+            $this->languageCode,
+            $this->countryCode
+        );
+    }
+
+    /**
+     * Get countryCode
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * Get languageCode
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @return string
+     */
+    public function getLanguageCode()
+    {
+        return $this->languageCode;
+    }
+
+    /**
+     * From string
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @param string $localeAsString
+     *
+     * @return Locale
+     */
+    public static function fromString($localeAsString)
+    {
+        $segments = explode('_', $localeAsString);
+
+        return new static($segments[0], $segments[1]);
+    }
+
+    /**
+     * From countryCode/languageCode
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     *
+     * @param string $countrySlashLanguage
+     *
+     * @return Locale
+     */
+    public static function fromCountrySlashLanguage($countrySlashLanguage)
+    {
+        $segments = explode('/', $countrySlashLanguage);
+
+        return new static($segments[1], $segments[0]);
+    }
+}
